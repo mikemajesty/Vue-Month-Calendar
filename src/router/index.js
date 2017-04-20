8f8045d044dcd9ebe063059f3226510b004b5ee9
mikemajesty <mikemajesty2011@gmail.com>
2017-04-19 21:55:55 -0300

    configure project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import MonthCalendar from '@/components/MonthCalendar'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'MonthCalendar',
+      component: MonthCalendar
     }
   ]
 })
