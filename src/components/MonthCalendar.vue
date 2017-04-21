8af103d37b1be0b787a85de0ac1b4585bcc39ca8
mikemajesty <mikemajesty2011@gmail.com>
2017-04-21 18:04:56 -0300

    configure vue js

--- src/components/MonthCalendar.vue
+++ src/components/MonthCalendar.vue
@@ -1,38 +1,50 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-  
-  </div>
-</template>
+<template lang="html">
+	<md-layout md-gutter>
+		<md-layout md-flex-small="100" md-flex-medium="100" md-hide-xsmall>
 
+			<md-input-container>
+				<label class="hello">{{ msg }}</label>
+        <md-input></md-input>
+			</md-input-container>
+
+		
+		</md-layout>
+	</md-layout>
+</template>
 <script>
-export default {
-  name: 'MonthCalendar',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Vue from 'vue'
+  import VueMaterial from 'vue-material'
+
+  Vue.use(VueMaterial)
+
+  export default {
+    name: 'MonthCalendar',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
     }
   }
-}
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
